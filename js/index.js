a62dc626347ec875a102035b27557ed15866d000
Hannah Galocy <hannahgalocy@Hannahs-MacBook-Pro.local>
2021-10-07 01:06:24 -0700

    implementing iframe and iframe resizer

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
-let $ = require( "jquery" );
+//adjust iframe size dynamically
+let options = {
+    log: true,
+    autoResize: true,
+}
+iFrameResize( options, '#myFrame' );
+
+const myFrame = document.getElementById('myFrame');
+document.getElementById("account-link").addEventListener("click", function(){
+    myFrame.src = "account.html";
+});
+document.getElementById("favorite-link").addEventListener("click", function(){
+    myFrame.src = "favorite.html";
+});
+
+const ban = document.getElementById("testugh")
+myFrame.onLoad = function (){
+    ban.style.backgroundColor = "red";
+
+}
+//myFrame.src = "main.html";
+//myFrame.src = "login.html";
+
 
