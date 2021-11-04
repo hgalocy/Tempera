bd814519d7e1d0f065406d07145e785395f8601d
Andrew <abirn@sdsu.edu>
2021-11-03 21:25:31 -0700

    Changes to php files: Created addUser and addItem functionality

---
+++ php/db_functions.php
@@ -0,0 +1,65 @@
+<?php
+include 'db_connection.php';
+
+/*
+ * varchar(40)  : userName, firstName, lastName, email, password
+ * varchar(10)  : phoneNumber
+ * varchar(64)  : delivery
+ * bool         : isArtist, specPainting, specDigitalPainting, specTextile, specEmbroidery, specPottery, ...
+ *                specSculpture
+ * text         : bio, location
+ * varchar(250) : responseMessage
+ * ???          : photo
+ */
+
+function AddUser($userName, $firstName, $lastName, $email, $phoneNumber, $isArtist, $password, $delivery, $photo,
+$bio, $location, $specPainting, $specDigitalPainting, $specTextile, $specEmbroidery, $specPottery, $specSculpture)
+{
+    //Connect to MySQL database
+    $conn = Connect();
+
+    $inputQuery = 'CALL tempera.addUser( \'' . $userName . '\', \'' . $firstName . '\', \'' . $lastName .
+        '\', \'' . $email . '\', \'' . $phoneNumber . '\', ' . $isArtist . ', \'' . $password . '\', \'' . $delivery .
+        '\', \'' . $photo . '\', \'' . $bio . '\', \'' . $location . '\', ' . $specPainting . ', ' .
+        $specDigitalPainting . ', ' . $specTextile . ', ' . $specEmbroidery . ', ' . $specPottery . ', ' .
+        $specSculpture . ', ' . '@responseMessage);';
+
+
+    echo $inputQuery;
+
+    //Call addUser function, input user data into account table of database
+    //mysqli_query($conn, $inputQuery);
+
+    CloseConnect($conn);
+}
+
+/*
+ * varchar(20)    : itemName
+ * varchar(40)    : username, password
+ * text           : itemDescription
+ * bool           : painting, pottery, textile, sculpture, embroidery
+ * decimal(65,2)  : price
+ * mediumBlob     : image
+ * varchar(250)   : responseMessage
+ */
+function AddItem( $itemName, $itemDescription, $painting, $digitalPainting, $pottery, $textile, $sculpture,
+                    $embroidery, $image, $price, $soldOut, $username, $password)
+{
+    //Connect to MySQL database
+    $conn = Connect();
+
+    $responseMessage = '';
+
+    $inputQuery = 'CALL addItem(\'' . $itemName . '\', \'' . $itemDescription . '\', ' . $painting . ', ' .
+        $digitalPainting . ', ' . $pottery . ', ' . $textile . ', ' . $sculpture . ', ' . $embroidery . ', \'' .
+        $image . '\', ' . $price . ', ' . $soldOut . ', \'' . $username . '\', \'' . $password .
+        '\', @responseMessage);';
+
+    echo $inputQuery;
+
+   mysqli_query($conn, $inputQuery);
+
+    CloseConnect($conn, $inputQuery);
+}
+
+?>
--- php/test_connection.php
+++ php/test_connection.php
@@ -1,10 +1,14 @@
 <?php
-include 'db_connection.php';
+include 'db_functions.php';
 
-$conn = Connect();
+$imageString = 'C:/Users/andre/Desktop/Jerry_Face.jpg';
 
-echo "Connected Successfully";
+AddUser('PHPEntry', 'PHP', 'Data', 'php@email.com', 1234567890,
+        1, 'SecretPassword', 'Send data via website', $imageString,
+          'Information added remotely', '123 Address Lane', 0, 1,
+     0, 0, 0, 0);
 
-CloseConnect($conn);
+AddItem('Beeg Stick', 'Like a stick... but bigger', 0, 0, 0, 0,
+        1, 0, $imageString, 199.99, 0, 'PHPEntry', 'SecretPassword');
 
 ?>
